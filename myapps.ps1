0973431eee0670373cbcc2a329496c7e801d2122
Andreas Jonsson <ajdev8@gmail.com>
2022-06-16 10:04:40 +0200

    Add script for default apps

---
+++ myapps.ps1
@@ -0,0 +1,49 @@
+# Default apps
+
+#   TODO:
+#     Not available:
+#       Moo0 System Monitor
+#       PureVPN
+#       SpiderOak
+#       Webtorrent
+
+# Communication
+winget install teams zoom
+
+# Dev
+winget install git dbbrowser vscode terminal
+
+# Downloads
+winget install qbittorrent
+
+# Files
+winget install 7zip dropbox wiztree
+
+# Library
+#   TODO: Check
+# winget install dotnet-sdk vcredist2022
+
+# Media
+winget install acrobatreader
+
+# Music
+winget install picard spotify
+
+# Video
+winget install "mpv (Unofficial)" plex-desktop vlc
+#mpv
+#winget install 9P3JFR0CLLL6 
+
+# Productivity
+winget install evernote
+
+# Tools
+winget install auto-dark-mode "AutoHotkey Store Edition" bitwarden "Barrier" ccleaner nmap nomachine powertoys "Microsoft Remote Desktop" virt-viewer
+#autohotkey
+#winget install 9NQ8Q8J78637
+
+# Web
+winget install googlechrome firefox
+
+# My bucket
+# winget install moo0-system-monitor
