9bf9e73bc5316a693f78c4398cfd37896ebdb3e5
Andreas Jonsson <ajdev8@gmail.com>
2022-06-21 10:45:37 +0200

    Use foreach loop to process each ComputerType

--- Install-AJ8Apps.psm1
+++ Install-AJ8Apps.psm1
@@ -16,91 +16,93 @@ function Install-AJ8Apps {
         [string[]]$ComputerType
     )
 
+    foreach ($Type in $ComputerType) {
 
-    switch ( $ComputerType )
-    {
-        "work"
+        switch ( $Type )
         {
-            $apps = @(
-                # Dev
-                @{name = "DBBrowserForSQLite.DBBrowserForSQLite"},
-                @{name = "Git.Git"},
-                @{name = "Microsoft.WindowsTerminal"},
-                @{name = "Microsoft.VisualStudioCode"},
-                # Files
-                @{name = "7zip.7zip"},
-                @{name = "Dropbox.Dropbox"},
-                @{name = "AntibodySoftware.WizTree"},
-                # Library
-                @{name = "Microsoft.DotNet.Runtime.6"},
-                @{name = "Microsoft.VC++2015-2022Redist-x64"},
-                # @{name = "Microsoft.VC++2015-2022Redist-x86"},
-                # Media
-                @{name = "Adobe.Acrobat.Reader.64-bit"},
-                # Tools
-                @{name = "Armin2208.WindowsAutoNightMode"},
-                @{name = "9NQ8Q8J78637"},   # AutoHotkey
-                @{name = "Bitwarden.Bitwarden"},
-                @{name = "DebaucheeOpenSourceGroup.Barrier"},
-                @{name = "Piriform.CCleaner"},
-                @{name = "Insecure.Nmap"},
-                # @{name = "moo0-system-monitor"},   # unavailable
-                @{name = "NoMachine.NoMachine"},
-                @{name = "Microsoft.PowerToys"},
-                @{name = "9WZDNCRFJ3PS"},   # Microsoft Remote Desktop
-                @{name = "RedHat.VirtViewer"},
-                # Web
-                @{name = "Mozilla.Firefox"},
-                @{name = "Google.Chrome"}
-            )
+            "work"
+            {
+                $apps = @(
+                    # Dev
+                    @{name = "DBBrowserForSQLite.DBBrowserForSQLite"},
+                    @{name = "Git.Git"},
+                    @{name = "Microsoft.WindowsTerminal"},
+                    @{name = "Microsoft.VisualStudioCode"},
+                    # Files
+                    @{name = "7zip.7zip"},
+                    @{name = "Dropbox.Dropbox"},
+                    @{name = "AntibodySoftware.WizTree"},
+                    # Library
+                    @{name = "Microsoft.DotNet.Runtime.6"},
+                    @{name = "Microsoft.VC++2015-2022Redist-x64"},
+                    # @{name = "Microsoft.VC++2015-2022Redist-x86"},
+                    # Media
+                    @{name = "Adobe.Acrobat.Reader.64-bit"},
+                    # Tools
+                    @{name = "Armin2208.WindowsAutoNightMode"},
+                    @{name = "9NQ8Q8J78637"},   # AutoHotkey
+                    @{name = "Bitwarden.Bitwarden"},
+                    @{name = "DebaucheeOpenSourceGroup.Barrier"},
+                    @{name = "Piriform.CCleaner"},
+                    @{name = "Insecure.Nmap"},
+                    # @{name = "moo0-system-monitor"},   # unavailable
+                    @{name = "NoMachine.NoMachine"},
+                    @{name = "Microsoft.PowerToys"},
+                    @{name = "9WZDNCRFJ3PS"},   # Microsoft Remote Desktop
+                    @{name = "RedHat.VirtViewer"},
+                    # Web
+                    @{name = "Mozilla.Firefox"},
+                    @{name = "Google.Chrome"}
+                )
+            }
+            "home"
+            {
+                $apps = @(
+                    # Communication
+                    @{name = "Microsoft.Teams"},
+                    @{name = "Zoom.Zoom"},
+                    # Downloads
+                    @{name = "qBittorrent.qBittorrent"},
+                    # @{name = "webtorrent"},   # unavailable
+                    # Music
+                    @{name = "Spotify.Spotify"},
+                    # Productivity
+                    @{name = "evernote.evernote"},
+                    # Video
+                    @{name = "9P3JFR0CLLL6"},   # mpv (Unofficial)
+                    @{name = "Plex.Plex"}, # Plex Desktop
+                    # @{name = "Plex.PlexMediaPlayer"}, # Plex TV
+                    @{name = "VideoLAN.VLC"}
+                )
+            }
+            "main"
+            {
+                $apps = @(
+                    # Media
+                    @{name = "calibre"},
+                    # Music
+                    @{name = "mediamonkey"},
+                    @{name = "MusicBrainz.Picard"},
+                    # Productivity
+                    @{name = "thunderbird"},
+                    # Tools
+                    @{name = "unifiedremote"},
+                    @{name = "virtualbox"},
+                    # Video
+                    @{name = "Plex.PlexMediaServer"}
+                )
+            }
         }
-        "home"
-        {
-            $apps = @(
-                # Communication
-                @{name = "Microsoft.Teams"},
-                @{name = "Zoom.Zoom"},
-                # Downloads
-                @{name = "qBittorrent.qBittorrent"},
-                # @{name = "webtorrent"},   # unavailable
-                # Music
-                @{name = "Spotify.Spotify"},
-                # Productivity
-                @{name = "evernote.evernote"},
-                # Video
-                @{name = "9P3JFR0CLLL6"},   # mpv (Unofficial)
-                @{name = "Plex.Plex"}, # Plex Desktop
-                # @{name = "Plex.PlexMediaPlayer"}, # Plex TV
-                @{name = "VideoLAN.VLC"}
-            )
-        }
-        "main"
-        {
-            $apps = @(
-                # Media
-                @{name = "calibre"},
-                # Music
-                @{name = "mediamonkey"},
-                @{name = "MusicBrainz.Picard"},
-                # Productivity
-                @{name = "thunderbird"},
-                # Tools
-                @{name = "unifiedremote"},
-                @{name = "virtualbox"},
-                # Video
-                @{name = "Plex.PlexMediaServer"}
-            )
-        }
-    }
 
-    Foreach ($app in $work) {
-        $listApp = winget list --exact --query $app.name
-        if (![String]::Join("", $listApp).Contains($app.name)) {
-            Write-host "`nInstalling: " $app.name
-            # winget install --exact --silent --accept-source-agreements --accept-package-agreements --id $app.name
-        }
-        else {
-            Write-host "`nSkipping: " $app.name " (already installed)"
+        foreach ($app in $apps) {
+            $listApp = winget list --exact --query $app.name
+            if (![String]::Join("", $listApp).Contains($app.name)) {
+                Write-host "`nInstalling: " $app.name
+                # winget install --exact --silent --accept-source-agreements --accept-package-agreements --id $app.name
+            }
+            else {
+                Write-host "`nSkipping: " $app.name " (already installed)"
+            }
         }
     }
 }
